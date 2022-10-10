4e41a3bb5e54a43b2fc36e3686b805d2b4f66db4
go4sanil <sanil.kollara@hotmail.com>
2022-10-10 14:00:12 -0400

    Assignment week 5

--- src/AddChocolates.java
+++ src/AddChocolates.java
@@ -1,20 +1,21 @@
+/*8.	Write a program that will keep adding 5 chocolates to a box which already has 27 chocolates and add a mechanism that number of chocolates should never go above 63*/
 
 public class AddChocolates {
 
 	public static void main(String[] args) {
 		int chocolateMaxLimit = 63;
-		int initialChocalateCount = 27;
+		int initialChocalateCount = 5;
+		int addChocolates = 5;
 
 		while (initialChocalateCount <= chocolateMaxLimit) {
-		if ((initialChocalateCount + 5) >= 63)
-		break;
-		else {
-		initialChocalateCount += 5;
-		System.out.println("Current Chocolate count is :" + initialChocalateCount);
-		}}
-		System.out.println("Chocolate Capacity reached");
+			if ((initialChocalateCount + addChocolates) >= 63)
+				break;
+			else {
+				initialChocalateCount += addChocolates;
+				System.out.println("Current Chocolate count is :" + initialChocalateCount);
+			}
 		}
+		System.out.println("Chocolate Capacity reached");
+	}
 
-
-	
 }
